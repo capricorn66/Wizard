97bdbcf15215db8141c489540d33e4bf689dfea0
michal <michterlecki@gmail.com>
2020-06-28 17:49:12 +0200

    JS v1.43

--- app/js/copyValue.js
+++ app/js/copyValue.js
@@ -3,13 +3,24 @@ import $ from "jquery";
 function copyValue(elem, id) {
     const $elem = $(elem);
     const copyText = document.getElementById(id);
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    document.execCommand("copy");
+    const eventSuccess = function() {
+        $elem.css('min-width', $elem.outerWidth() );
+        $elem.css('flex', '0 0 ' + $elem.outerWidth() );
+        $elem.find('.event-ready').hide();
+        $elem.find('.event-success').show();
+        setTimeout( eventReady, 3000);
+    }
+    const eventReady = function() {
+        $elem.find('.event-ready').show();
+        $elem.find('.event-success').hide();
+    }
 
-    $elem.find('.event').hide();
-    $elem.find('.event-success').show();
-
+    if( copyText.value.length ) {
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        setTimeout( eventSuccess, 300);
+    }
 }
 
 export default copyValue;
