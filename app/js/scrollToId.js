fb2297c44b25d62f6c9f55d637b44d8da64574a4
michal <michterlecki@gmail.com>
2020-06-28 19:19:38 +0200

    RWD v1.44

--- app/js/scrollToId.js
+++ app/js/scrollToId.js
@@ -1,7 +1,8 @@
 import $ from "jquery";
 
 function scrollToId(elem) {
-    $("html, body").delay(200).animate({scrollTop: $(elem).offset().top - 100 }, 1000);
+    const scrollPos = rwdMedia.xs() || rwdMedia.sm() ? $(elem).offset().top - 20 : $(elem).offset().top - 100;
+    $("html, body").delay(200).animate({scrollTop: scrollPos }, 1000);
 }
 
 export default scrollToId;
